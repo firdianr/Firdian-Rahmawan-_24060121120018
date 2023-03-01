8efb789294800f757847a0241d0af9523d40aaab
firdianr <100133856+firdianr@users.noreply.github.com>
2023-03-01 20:18:19 +0700

    Update MOperasiTitik.java

--- PRAKTIKUM2/MOperasiTitik.java
+++ PRAKTIKUM2/MOperasiTitik.java
@@ -8,7 +8,7 @@
 
 class MOperasiTitik {
   public static void main(String[] args){
-    Titik t1 = new Titik(4.0, 2.0);
+    Titik t1 = new Titik(1.0, 2.0);
     OperasiTitik op = new OperasiTitik();
 
     System.out.println("t1("+ t1.getAbsis() + ", " + t1.getOrdinat() + ")");
