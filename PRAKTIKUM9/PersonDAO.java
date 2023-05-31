8d8330bf54e595943bf25e10e4d2ecda74af9f42
firdianr <100133856+firdianr@users.noreply.github.com>
2023-05-31 21:04:06 +0700

    Add files via upload

---
+++ PRAKTIKUM9/PersonDAO.java
@@ -0,0 +1,10 @@
+/*
+ * File		: PersonDAO.java	31 Mei 2023
+ * Penulis	: Firdian Rahmawan (24060121120018)
+ * Deskripsi	: interface untuk person access object
+ *
+*/
+
+public interface PersonDAO{
+	public void savePerson(Person p) throws Exception;
+}
